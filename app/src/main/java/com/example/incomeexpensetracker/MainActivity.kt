23535f1244521968f3888c8b2d5d2d1a4e10c319
anjuman ara mimi <anjumana0@gmail.com>
2022-02-28 16:28:57 +0600

    navigation-compose dependencies added

--- app/src/main/java/com/example/incomeexpensetracker/MainActivity.kt
+++ app/src/main/java/com/example/incomeexpensetracker/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.incomeexpensetracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.incomeexpensetracker.ui.theme.IncomeExpenseTrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,6 +38,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
+    
+    Card(modifier = Modifier.padding(24.dp)) {
+        Row() {
+
+            Column() {
+                Text(text = "Column One")
+            }
+
+            Column() {
+                Text(text = "Column Two")
+            }
+
+        }
+        
+    }
+    
 }
 
 @Preview(showBackground = true)
