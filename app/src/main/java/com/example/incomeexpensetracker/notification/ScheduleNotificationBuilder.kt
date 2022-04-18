1a3291b63b4471fc4ff2775efc2d910890077b12
anjuman ara mimi <anjumana0@gmail.com>
2022-04-18 19:34:59 +0600

    Notification Builder Refactored

--- app/src/main/java/com/example/incomeexpensetracker/notification/ScheduleNotificationBuilder.kt
+++ app/src/main/java/com/example/incomeexpensetracker/notification/ScheduleNotificationBuilder.kt
@@ -1,27 +1,20 @@
 package com.example.incomeexpensetracker.notification
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.example.incomeexpensetracker.MainActivity
 import com.example.incomeexpensetracker.R
 
 class ScheduleNotificationBuilder {
 
-    // Main steps for building a notification:
-    //      0. Get your data
-    //      1. Create/Retrieve Notification Channel for O and beyond devices (26+)
-    //      2. Build the Text Style
-    //      3. Set up main Intent for notification
-    //      4. Create additional Actions for the Notification
-    //      5. Build and issue the notification
-
     // variables
     private val channelID: String = "Scheduled_Transactions_Channel_Id"
-
-    // 0. Get your data
-    // TODO
 
     // 1. Create/Retrieve Notification Channel
     // Because it's necessary to set a notification channel to deliver notification on android 8.0 or higher,
@@ -37,34 +30,33 @@ class ScheduleNotificationBuilder {
         val notificationManager: NotificationManager =
             context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
-
     }
 
-    //  2. Build the Text Style
-    // TODO
+    // 2. Set up main Intent for notification
+    private fun getPendingIntent(context: Context): PendingIntent {
+        val intent = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+    }
 
-    // 3. Set up main Intent for notification
-    // TODO
-
-    // 4. Create additional Actions for the Notification
-    // TODO
-
-    //  5. Build and issue the notification
-    private fun getBuilder(context: Context, channelID: String): NotificationCompat.Builder {
+    //  3. Build the notification
+    private fun getNotification(context: Context, channelID: String): Notification {
         return NotificationCompat.Builder(context, channelID)
             .setSmallIcon(R.drawable.ic_baseline_add_alert_24)
             .setContentTitle("Schedule Transaction")
-            .setContentText("You have a Pending Transaction")
+            .setContentText("You have Pending Transactions")
             .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(getPendingIntent(context = context))
             .setAutoCancel(true)
+            .build()
     }
 
+    // 4. Show the Notification
     fun showNotification(context: Context) {
-
-        val builder = getBuilder(context = context, channelID = channelID)
-        with(NotificationManagerCompat.from(context)) {
-            // notificationId is a unique int for each notification that you must define
-            notify(123, builder.build())
-        }
+        val notificationId = 1
+        val notification = getNotification(context = context, channelID = channelID)
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(notificationId, notification)
     }
 }
