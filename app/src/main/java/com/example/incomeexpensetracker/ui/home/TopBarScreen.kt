ed8a7930adaf723e18799a7a74db644c38800fff
anjuman ara mimi <anjumana0@gmail.com>
2022-04-19 01:02:29 +0600

    home top bar screen modified

--- app/src/main/java/com/example/incomeexpensetracker/ui/home/TopBarScreen.kt
+++ app/src/main/java/com/example/incomeexpensetracker/ui/home/TopBarScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -12,13 +13,16 @@ fun topBarScreen() {
 
     TopAppBar(
         title = {
-            Text(text = "Income-Expense Tracker")
+            Text(
+                text = "Income-Expense Tracker",
+                textAlign = TextAlign.Center
+            )
         },
-        navigationIcon = {
-            IconButton(onClick = {}) {
-                Icon(Icons.Filled.List, "ListIcon")
-            }
-        },
+//        navigationIcon = {
+//            IconButton(onClick = {}) {
+//                Icon(Icons.Filled.List, "ListIcon")
+//            }
+//        },
         backgroundColor = MaterialTheme.colors.primaryVariant,
         contentColor = Color.White,
         elevation = 10.dp
