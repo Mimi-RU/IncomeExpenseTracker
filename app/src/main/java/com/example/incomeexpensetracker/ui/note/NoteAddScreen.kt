5fb2298ae1aff711bd4f86d463a70e4703307bab
anjuman ara mimi <anjumana0@gmail.com>
2022-04-19 01:29:37 +0600

    Note List adding done.

--- app/src/main/java/com/example/incomeexpensetracker/ui/note/NoteAddScreen.kt
+++ app/src/main/java/com/example/incomeexpensetracker/ui/note/NoteAddScreen.kt
@@ -1,9 +1,87 @@
 package com.example.incomeexpensetracker.ui.note
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.incomeexpensetracker.nav_routes
+import com.example.incomeexpensetracker.ui.components.enterText
 
 @Composable
-fun noteAddScreen(navHostController: NavHostController){
+fun noteAddScreen(navHostController: NavHostController) {
+    val noteViewModel: NoteViewModel = hiltViewModel()
 
+    val note by noteViewModel.note
+
+    Scaffold(
+        topBar = {
+            noteAddTopBar(
+                navHostController = navHostController,
+                noteViewModel = noteViewModel
+            )
+        }
+    )
+    {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+
+
+            // name
+            enterText(
+                label = "Note",
+                value = note,
+                onValueChange = {
+                    noteViewModel.note.value = it
+                }
+            )
+        }
+
+    }
+
+
+}
+
+@Composable
+fun noteAddTopBar(navHostController: NavHostController, noteViewModel: NoteViewModel) {
+    TopAppBar(
+        navigationIcon = {
+            IconButton(onClick = { navHostController.navigate(nav_routes.note_list) }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back to Note List"
+                )
+            }
+        },
+        title = {
+            Text(text = "Add Note")
+        },
+        actions = {
+
+            IconButton(onClick = {
+                noteViewModel.storeNote()
+                val pr = navHostController.previousBackStackEntry?.destination?.route
+                navHostController.navigate(pr ?: nav_routes.note_list)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Check,
+                    contentDescription = "Save Note"
+                )
+            }
+
+        }
+
+    )
 }
--- app/src/main/java/com/example/incomeexpensetracker/ui/note/NoteListScreen.kt
+++ app/src/main/java/com/example/incomeexpensetracker/ui/note/NoteListScreen.kt
@@ -1,9 +1,87 @@
 package com.example.incomeexpensetracker.ui.note
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.example.incomeexpensetracker.data.model.Note
+import com.example.incomeexpensetracker.nav_routes
+import com.example.incomeexpensetracker.ui.components.backTOHomeTopAppBar
 
 @Composable
 fun noteListScreen(navHostController: NavHostController) {
+    val noteViewModel: NoteViewModel = hiltViewModel()
+
+    LaunchedEffect(key1 = true) {
+        noteViewModel.getAllNotes()
+    }
+    val noteListState = noteViewModel.allNotes.collectAsState()
+    val noteList = noteListState.value
+
+    Scaffold(
+        topBar = {
+            backTOHomeTopAppBar(
+                title = "Notes",
+                navHostController = navHostController
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                navHostController.navigate(nav_routes.note_add)
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Note"
+                )
+            }
+        }
+    )
+    {
+
+        LazyColumn(
+            contentPadding = PaddingValues(bottom = 16.dp)
+        ) {
+            items(noteList) { item: Note ->
+                noteItem(note = item, navHostController = navHostController)
+            }
+        }
+
+    }
+}
+
+@Composable
+fun noteItem(note: Note, navHostController: NavHostController) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp)
+            .clickable { navHostController.navigate("note_edit/${note.id}") }
+    ) {
+        Text(
+            text = note.note,
+            modifier = Modifier
+                .weight(0.5f),
+            textAlign = TextAlign.Center
+        )
+
+    }
+
+    Divider()
 
 }
+
+
