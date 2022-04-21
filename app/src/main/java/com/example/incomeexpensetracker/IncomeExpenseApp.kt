4051090081b98f9546f935616b02748579b2b05f
anjuman ara mimi <anjumana0@gmail.com>
2022-04-22 01:51:45 +0600

    need di to Worker

--- app/src/main/java/com/example/incomeexpensetracker/IncomeExpenseApp.kt
+++ app/src/main/java/com/example/incomeexpensetracker/IncomeExpenseApp.kt
@@ -1,7 +1,10 @@
 package com.example.incomeexpensetracker
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class IncomeExpenseApp : Application()
